aaca0f549afb1ac18ee0f20b792f629f62890483
amipatil <iamitpatil1993@gmail.com>
2020-04-16 23:30:04 +0530

    Initial Commit:Initial Project Setup

---
+++ src/main/java/org/code4cause/security/vault/springcloudvault/SpringCloudVaultApplication.java
@@ -0,0 +1,13 @@
+package org.code4cause.security.vault.springcloudvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/code4cause/security/vault/springcloudvault/SpringCloudVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code4cause.security.vault.springcloudvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
